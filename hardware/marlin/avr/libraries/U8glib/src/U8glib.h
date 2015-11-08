0926de4376194a416b7eec62922994636261cbc9
Richard Wackerbarth <rkw@dataplex.net>
2015-11-08 13:13:07 -0600

    Reposition U8glib

--- hardware/marlin/avr/libraries/U8glib/src/U8glib.h
+++ hardware/marlin/avr/libraries/U8glib/src/U8glib.h
@@ -39,7 +39,7 @@
 #define _CPP_U8GLIB
 
 #include <Print.h>
-#include "utility/u8g.h"
+#include "clib/u8g.h"
 
 
 class U8GLIB : public Print
@@ -83,13 +83,14 @@ class U8GLIB : public Print
       
     void setPrintPos(u8g_uint_t x, u8g_uint_t y) { tx = x; ty = y; }
     u8g_t *getU8g(void) { return &u8g; }
-    
+    u8g_uint_t getPrintCol(void) { return tx; }	
+    u8g_uint_t getPrintRow(void) { return ty; }    
     
     /* implementation of the write interface to the print class */
-#if defined(ARDUINO) && ARDUINO >= 100
-    size_t write(uint8_t c) { tx += u8g_DrawGlyph(&u8g, tx, ty, c); return 1;}
-#else
+#if defined(ARDUINO) && ARDUINO < 100
     void write(uint8_t c) { tx += u8g_DrawGlyph(&u8g, tx, ty, c); }
+#else
+    size_t write(uint8_t c) { tx += u8g_DrawGlyph(&u8g, tx, ty, c); return 1;}
 #endif
     
      /* screen rotation */
@@ -506,6 +507,10 @@ class U8GLIB_UC1611_DOGM240 : public U8GLIB
     U8GLIB_UC1611_DOGM240(uint8_t cs, uint8_t a0, uint8_t reset = U8G_PIN_NONE) 
       : U8GLIB(&u8g_dev_uc1611_dogm240_hw_spi, cs, a0, reset)
       { }
+    U8GLIB_UC1611_DOGM240(uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7, 
+        uint8_t en, uint8_t cs1, uint8_t di, uint8_t rw = U8G_PIN_NONE, uint8_t reset = U8G_PIN_NONE) 
+      : U8GLIB(&u8g_dev_uc1611_dogm240_8bit, d0, d1, d2, d3, d4, d5, d6, d7, en, cs1, U8G_PIN_NONE, di, rw, reset)  // cs2 = U8G_PIN_NONE
+      { }
 };
 
 class U8GLIB_UC1611_DOGXL240 : public U8GLIB
@@ -519,6 +524,10 @@ class U8GLIB_UC1611_DOGXL240 : public U8GLIB
       { }
     U8GLIB_UC1611_DOGXL240(uint8_t cs, uint8_t a0, uint8_t reset = U8G_PIN_NONE) 
       : U8GLIB(&u8g_dev_uc1611_dogxl240_hw_spi, cs, a0, reset)
+      { }
+    U8GLIB_UC1611_DOGXL240(uint8_t d0, uint8_t d1, uint8_t d2, uint8_t d3, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7, 
+        uint8_t en, uint8_t cs1, uint8_t di, uint8_t rw = U8G_PIN_NONE, uint8_t reset = U8G_PIN_NONE) 
+      : U8GLIB(&u8g_dev_uc1611_dogxl240_8bit, d0, d1, d2, d3, d4, d5, d6, d7, en, cs1, U8G_PIN_NONE, di, rw, reset)  // cs2 = U8G_PIN_NONE
       { }
 };
 
@@ -706,7 +715,7 @@ class U8GLIB_LC7981_240X128 : public U8GLIB
       { }
 };
 
-// 16 bit mode required: Remove comment from "#define U8G_16BIT 1" in utility/utility/u8g.h
+// 16 bit mode required: Remove comment from "#define U8G_16BIT 1" in utility/clib/u8g.h
 class U8GLIB_LC7981_320X64 : public U8GLIB 
 {
   public:
@@ -956,6 +965,34 @@ class U8GLIB_SSD1306_128X32_2X : public U8GLIB
       { }
     U8GLIB_SSD1306_128X32_2X(uint8_t options = U8G_I2C_OPT_NONE) 
       : U8GLIB(&u8g_dev_ssd1306_128x32_2x_i2c, options)
+      { }
+};
+
+class U8GLIB_SSD1306_64X48 : public U8GLIB 
+{
+  public:
+    U8GLIB_SSD1306_64X48(uint8_t sck, uint8_t mosi, uint8_t cs, uint8_t a0, uint8_t reset = U8G_PIN_NONE) 
+      : U8GLIB(&u8g_dev_ssd1306_64x48_sw_spi, sck, mosi, cs, a0, reset)
+      { }
+    U8GLIB_SSD1306_64X48(uint8_t cs, uint8_t a0, uint8_t reset = U8G_PIN_NONE) 
+      : U8GLIB(&u8g_dev_ssd1306_64x48_hw_spi, cs, a0, reset)
+      { }
+    U8GLIB_SSD1306_64X48(uint8_t options = U8G_I2C_OPT_NONE) 
+      : U8GLIB(&u8g_dev_ssd1306_64x48_i2c, options)
+      { }
+};
+
+class U8GLIB_SSD1306_64X48_2X : public U8GLIB 
+{
+  public:
+    U8GLIB_SSD1306_64X48_2X(uint8_t sck, uint8_t mosi, uint8_t cs, uint8_t a0, uint8_t reset = U8G_PIN_NONE) 
+      : U8GLIB(&u8g_dev_ssd1306_64x48_2x_sw_spi, sck, mosi, cs, a0, reset)
+      { }
+    U8GLIB_SSD1306_64X48_2X(uint8_t cs, uint8_t a0, uint8_t reset = U8G_PIN_NONE) 
+      : U8GLIB(&u8g_dev_ssd1306_64x48_2x_hw_spi, cs, a0, reset)
+      { }
+    U8GLIB_SSD1306_64X48_2X(uint8_t options = U8G_I2C_OPT_NONE) 
+      : U8GLIB(&u8g_dev_ssd1306_64x48_2x_i2c, options)
       { }
 };
 
